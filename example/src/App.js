c8611d8aa6f262a5168028a629b6a647ebb3fa93
elsa.chen <elsa@surfside.io>
2022-01-25 10:59:47 -0500

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import { Upload, Button } from 'antd'
+import 'antd/dist/antd.css';
+import { UploadOutlined } from '@ant-design/icons';
 
-import { ExampleComponent } from 'antd-img-crop-resize'
+import ImgCrop from 'antd-img-crop-resize'
 import 'antd-img-crop-resize/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  const cropProps = {
+    modalTitle: "Creative Size",
+    aspect: 1/2,
+    // beforeCrop: () => {
+    //   if (!(height && width)) {
+    //     message.error({
+    //       content: "Please select Creative size before uploading image",
+    //       style: {
+    //         marginTop: '20vh',
+    //         marginLeft: '80vw',
+    //       },
+    //     });
+
+    //     return false
+    //   }
+    //   return true
+    // }
+  }
+
+  return (
+    <>
+    <ImgCrop {...cropProps}>
+      <Upload
+        accept=".gif, .jpeg, .png"
+        maxCount={1}
+        progress={{
+          strokeColor: {
+            '0%': '#108ee9',
+            '100%': '#87d068',
+          },
+          strokeWidth: 3,
+          format: percent => `${parseFloat(percent.toFixed(2))}%`,
+        }}>
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload> 
+    </ImgCrop>
+    </>
+  )
+
+
 }
 
 export default App
